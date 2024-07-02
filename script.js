c29de25df23c5540520f793768b3b117997877b3
Corfaya <lauracorrado0696@gmail.com>
2024-07-02 15:07:51 +0200

    [UPD] personal exercise (push ingredients) - add a click event on btn

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Variables
+// --- VARIABLES ---
 const ingredients = ["Digestive", "Burro", "Zucchero", "Limone", "Formaggio spalmabile", "Panna fresca", "Zucchero a velo", "Gelatina", "Vaniglia", "Frutti di bosco", "Marmellata"]
 const shoppingList = document.getElementById("shopping-list")
 
-// Logic
+// --- LOGIC ---
 let i = 0
 while (i < ingredients.length) {
     let li = document.createElement("li")
@@ -10,3 +10,8 @@ while (i < ingredients.length) {
     shoppingList.append(li)
     i++
 }
+
+// --- PERSONAL EXERCISES ---
+document.getElementById("add-ingredients").addEventListener("click", function() {
+    
+})
