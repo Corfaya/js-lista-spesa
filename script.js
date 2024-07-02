3bc2f605c4b3fd99609de09e7e1586c9626ad3ff
Corfaya <lauracorrado0696@gmail.com>
2024-07-02 14:55:16 +0200

    [UPD] ul added on HTML file; ul recovered on JS file

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
+// Variables
 const ingredients = ["Digestive", "Burro", "Zucchero", "Limone", "Formaggio spalmabile", "Panna fresca", "Zucchero a velo", "Gelatina", "Vaniglia", "Frutti di bosco", "Marmellata"]
+const shoppingList = document.getElementById("shopping-list")
