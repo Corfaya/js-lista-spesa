b39b70e067e848db414a3925fceaff26192153d8
Corfaya <lauracorrado0696@gmail.com>
2024-07-02 14:57:06 +0200

    [UPD] list item creation in DOM saved in a var (for each iteration)

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // Variables
 const ingredients = ["Digestive", "Burro", "Zucchero", "Limone", "Formaggio spalmabile", "Panna fresca", "Zucchero a velo", "Gelatina", "Vaniglia", "Frutti di bosco", "Marmellata"]
 const shoppingList = document.getElementById("shopping-list")
+
+// Logic
+let i = 0
+while (i < ingredients.length) {
+    let li = document.createElement("li")
+    
+}
