9dc21fbd2e701d6d3671638137d0a63ac7905ad1
Corfaya <lauracorrado0696@gmail.com>
2024-07-03 09:31:45 +0200

    fix: if newIngredient is empty, don't add anything and send an alert message

--- script.js
+++ script.js
@@ -14,8 +14,12 @@ while (i < ingredients.length) {
 // --- PERSONAL EXERCISES ---
 document.getElementById("add-ingredients").addEventListener("click", function() {
     let newIngredient = document.getElementById("ingredients").value;
-    ingredients.push(newIngredient)
-    let li = document.createElement("li")
-    li.innerHTML = newIngredient;
-    shoppingList.append(li)
+    if (newIngredient === "") {
+        alert("Aggiungi un ingrediente")
+    } else {
+        ingredients.push(newIngredient)
+        let li = document.createElement("li")
+        li.innerHTML = newIngredient;
+        shoppingList.append(li)
+    }
 })
